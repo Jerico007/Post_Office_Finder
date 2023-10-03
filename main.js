d5f6fdee1dbcb255c5ce32aae1599ef0f2ae4887
Jerico <jericodcosta95375788@gmail.com>
2023-10-03 23:35:29 +0530

    First commit

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const dateTime = document.getElementById("date-time");
 const pincode = document.getElementById("pincode");
 const message = document.getElementById("message");
 const cardContainer = document.getElementsByClassName("card-container")[0];
+const search = document.getElementById("search");
 let data = localStorage.getItem("Apidata");
 
 data = JSON.parse(data);
@@ -41,6 +42,7 @@ timeZone.innerText = data.timezone;
 dateTime.innerText = India_datetime_str;
 pincode.innerText = data.postal;
 
+//Function to fetchPincode data
 async function fetchPincodeData() {
   url = `https://api.postalpincode.in/pincode/${data.postal}`;
 
@@ -65,3 +67,41 @@ function addPostDetailsInUI(postOffices) {
 }
 
 fetchPincodeData();
+
+//Functionality to reset the cards after searching
+function resetDetails(cards) {
+  Array.from(cards).forEach((val) => {
+    val.style.display = "flex";
+  });
+}
+
+//Search Functionality
+function searchDetails(str) {
+  let cards = cardContainer.children;
+
+  if (cards.length === 0 || str === "") {
+    resetDetails(cards);
+    return;
+  }
+  //Reseting cards before a search
+  resetDetails(cards);
+  Array.from(cards).forEach((val) => {
+    let name = val.children[0].innerText;
+    let BranchType = val.children[1].innerText;
+    name = name.substring(0, str.length);
+    BranchType = BranchType.substring(0, str.length);
+    if (
+      name.toUpperCase().trim() === str.toUpperCase().trim() ||
+      BranchType.toUpperCase().trim() === str.toUpperCase().trim()
+    ) {
+      val.style.display = "";
+    } else {
+      val.style.display = "none";
+    }
+  });
+}
+
+//Adding eventListener to search input
+search.addEventListener("input", (e) => {
+  searchDetails(e.target.value.trim());
+});
